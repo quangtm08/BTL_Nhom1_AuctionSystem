280a3fdbc63aa210637ffb074a0bb55f305f93d5
Nguyễn Đức Duy <132185526+NguyenDuyHanThuyen@users.noreply.github.com>
2026-03-31 21:45:30 +0700

    "add authentication controllers and update navigation with dependencies"

--- src/main/java/com/nhom1/auction/client/AppView.java
+++ src/main/java/com/nhom1/auction/client/AppView.java
@@ -1,5 +1,25 @@
 package com.nhom1.auction.client;
 
-public class AppView {
+public enum AppView {
 
+    SIGN_IN("/views/sign_in.fxml", null),
+    MAIN_DASHBOARD("/views/main_dashboard_explore.fxml",null),
+    LOADING("/views/loading.fxml", null),
+    REGISTER("/views/register.fxml",null);
+
+    private final String fxml;
+    private final String css;
+
+    AppView(String fxml, String css) {
+        this.fxml = fxml;
+        this.css = css;
+    }
+
+    public String getFxml() {
+        return fxml;
+    }
+
+    public String getCss() {
+        return css;
+    }
 }
