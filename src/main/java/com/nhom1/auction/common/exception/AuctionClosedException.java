fed413acf5f38dda775cdfe0c9828fa580b50321
Nguyễn Hoàng Minh Ngọc <minhngocnhap@gmail.com>
2026-04-01 11:29:02 +0700

    add Exceptions

---
+++ src/main/java/com/nhom1/auction/common/exception/AuctionClosedException.java
@@ -0,0 +1,10 @@
+package com.nhom1.auction.common.exception;
+
+public class AuctionClosedException extends Exception {
+
+    public AuctionClosedException(String message) {
+        super(message);
+    }
+
+}
+
