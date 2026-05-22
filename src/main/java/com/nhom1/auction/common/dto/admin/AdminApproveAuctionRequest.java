d24fa3b29aa461339b7cdbb7e3dac191bdfe8e1d
Nguyễn Đức Duy <132185526+NguyenDuyHanThuyen@users.noreply.github.com>
2026-05-22 09:52:11 +0700

    chore(validation): add/update AdminApproveAuctionRequest and AuctionBidValidator

--- src/main/java/com/nhom1/auction/common/dto/admin/AdminApproveAuctionRequest.java
+++ src/main/java/com/nhom1/auction/common/dto/admin/AdminApproveAuctionRequest.java
@@ -3,12 +3,18 @@ package com.nhom1.auction.common.dto.admin;
 public class AdminApproveAuctionRequest {
     private String auctionId;
     private String callerId;
+    private String openingDate;
 
     public AdminApproveAuctionRequest() {}
 
     public AdminApproveAuctionRequest(String auctionId, String callerId) {
+        this(auctionId, callerId, null);
+    }
+
+    public AdminApproveAuctionRequest(String auctionId, String callerId, String openingDate) {
         this.auctionId = auctionId;
         this.callerId = callerId;
+        this.openingDate = openingDate;
     }
 
     public String getAuctionId() {
@@ -25,5 +31,13 @@ public class AdminApproveAuctionRequest {
 
     public void setCallerId(String callerId) {
         this.callerId = callerId;
+    }
+
+    public String getOpeningDate() {
+        return openingDate;
+    }
+
+    public void setOpeningDate(String openingDate) {
+        this.openingDate = openingDate;
     }
 }
